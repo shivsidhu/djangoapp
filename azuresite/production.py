b31a4066a929336e1de556689f7529b55e341bd5
Shiv Sidhu <shsidhu@microsoft.com>
2020-08-10 16:28:49 +0530

    test

--- azuresite/production.py
+++ azuresite/production.py
@@ -1,5 +1,5 @@
 from .settings import *
-
+#
 # Configure default domain name
 ALLOWED_HOSTS = [os.environ['WEBSITE_SITE_NAME'] + '.azurewebsites.net', '127.0.0.1'] if 'WEBSITE_SITE_NAME' in os.environ else []
 
